a487322e28791354546ffe4497e863624cc1e34c
Sebastian Schmidt <sebastian.schmidt@helsinki.fi>
2025-04-01 09:30:32 +0300

    Format code.

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,5 @@
 //! An alternative to the standard libraries' [`Vec`] which is indexed with a custom type instead of [`usize`].
-//! 
+//!
 //! This is useful to catch errors like using the wrong variable to index the vector.
 
 #![deny(missing_docs)]
